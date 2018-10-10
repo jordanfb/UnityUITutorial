133e5ac704ebed96c5019a34726d405b92a9c6d7
Jordan Faas-Bush <jmanfb@gmail.com>
2018-10-10 00:44:36 -0400

    Added camera aiming script

---
+++ Assets/Scripts/PointCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCamera : MonoBehaviour {
+
+    Quaternion startLoc;
+    Quaternion goalLoc;
+    private float lerpTime = 0;
+    [SerializeField]
+    private float time = 1;
+
+    // Use this for initialization
+    void Start () {
+        goalLoc = transform.rotation;
+	}
+	
+    public void SetTarget(Vector3 pos)
+    {
+        // aim the camera at the pos
+        startLoc = transform.rotation;
+        Vector3 dpos = pos - transform.position;
+        goalLoc = Quaternion.LookRotation(dpos, Vector3.up);
+        lerpTime = 0;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        lerpTime += Time.deltaTime / time;
+        transform.rotation = Quaternion.Lerp(startLoc, goalLoc, lerpTime / time);
+	}
+}
---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour {
+
+    [SerializeField]
+    List<Transform> worldPositions;
+    int i = 0;
+
+    [SerializeField]
+    PointCamera pointCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+            pointCamera.SetTarget(worldPositions[i].position);
+            i++;
+        }
+	}
+}
